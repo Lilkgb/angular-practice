b68a0ff904728f75bc8ce48a2feb632100eb339d
Student <example@example.com>
2019-03-28 14:43:45 -0700

    practice to do list. practice seperation of logic in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './model/task.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+
+  tasks: Task [] = [
+    new Task("Finish Homework", 3),
+    new Task("Not go crazy", 2),
+    new Task("Play Fortnite", 1)
+  ];
+
+  selectedTask = null;
+
+  editTask(clickedTask){
+    this.selectedTask = clickedTask
+  }
+  finishEditing(){
+    this.selectedTask = null;
+  }
+
+  priorityColor(currentTask){
+    if (currentTask.priority === 3 || currentTask.priority === '3'){
+      return "red";
+    } else if (currentTask.priority === 2 || currentTask.priority === '2'){
+      return "yellow";
+    } else {
+      return "green";
+    }
+  }
 }
